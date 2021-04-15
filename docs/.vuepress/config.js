bead0e579ebf586c27891b856ce534a3f9db4980
Karolis Breidokas <karolis@martynas.it>
2021-04-15 18:43:32 +0300

    Add `sidebar: auto` fontmatter

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+
+
+
+module.exports = {
+    lang: 'en-US',
+    title: 'Hello, VuePress!',
+    description: 'This is my first VuePress site',
+  
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+      sidebar: false,
+    },
+  }
+  
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,7 @@
+---
+sidebar: auto
+---
+
 # Hello VuePress
 
 ## "test1"
